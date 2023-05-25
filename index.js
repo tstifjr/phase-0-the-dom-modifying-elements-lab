f4769dda94a5e13799da0e9f3e71c4219dcc4bb3
TJ Stifter <tjstifter@gmail.com>
2023-05-25 10:20:32 -0400

    completes test and on branch

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ element.remove();
 
 let newHeader = document.createElement("h1");
 console.log(newHeader)
+
+newHeader.id = "victory";
+
+newHeader.textContent= "TJ is the champion";
+console.log(newHeader)
+newHeader.innerText= "Megan is the champion";
+console.log(newHeader)
